15040a6ed266a6476bd42831891c7131bd7d5d2e
spencershepard <spencershepard@hotmail.com>
2023-10-19 21:07:58 -0700

    Create update_packs.sh

---
+++ update_packs.sh
@@ -0,0 +1,21 @@
+echo "Choose level to update with packs (or "all"): "
+ls mcbs/worlds
+read level_name
+
+if [ "$level_name" = "all" ]
+then
+    for level in mcbs/worlds/*
+    do
+        if [ -d "$level" ]
+        then
+            echo "Updating packs for $level"
+            cp world_resource_packs.json $level
+            cp -r behavior_packs $level/behavior_packs
+            cp -r resource_packs $level/resource_packs
+        fi
+    done
+else
+    cp world_resource_packs.json mcbs/worlds/$level_name
+    cp -r behavior_packs mcbs/worlds/$level_name/behavior_packs
+    cp -r resource_packs mcbs/worlds/$level_name/resource_packs
+fi
