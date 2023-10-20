f048877efdb2a01fc365c5d3711f64944be61c1e
spencershepard <spencershepard@hotmail.com>
2023-10-19 21:16:20 -0700

    Update update_packs.sh

--- update_packs.sh
+++ update_packs.sh
@@ -9,13 +9,13 @@ then
         if [ -d "$level" ]
         then
             echo "Updating packs for $level"
-            cp world_resource_packs.json $level
-            cp -r behavior_packs $level/behavior_packs
-            cp -r resource_packs $level/resource_packs
+            cp world_resource_packs.json "$level"
+            cp -r behavior_packs "$level/behavior_packs"
+            cp -r resource_packs "$level"/resource_packs"
         fi
     done
 else
-    cp world_resource_packs.json mcbs/worlds/$level_name
-    cp -r behavior_packs mcbs/worlds/$level_name/behavior_packs
-    cp -r resource_packs mcbs/worlds/$level_name/resource_packs
+    cp world_resource_packs.json "mcbs/worlds/$level_name"
+    cp -r behavior_packs "mcbs/worlds/$level_name/behavior_packs"
+    cp -r resource_packs "mcbs/worlds/$level_name/resource_packs"
 fi
